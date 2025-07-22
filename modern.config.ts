ec6a92548d95a9ae4fc81ffb6acf8f782b09d275
Black Cat <rborole06@gmail.com>
2025-07-22 00:19:20 +0000

    Update modern.config.ts using env variables

--- modern.config.ts
+++ modern.config.ts
@@ -1,7 +1,24 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
 import { moduleFederationPlugin } from '@module-federation/modern-js';
 
+const loginCodespaceName = process.env.LOGIN_APP_CODESPACE_SUBDOMAIN;
+const loginPort = Number.parseInt(process.env.LOGIN_APP_PORT || '3051', 10);
+
 export default defineConfig({
+  // tools: {
+  //   devServer:{
+  //     // headers: {
+  //     //   'Access-Control-Allow-Origin': 'https://<github_codespace_subdomain>-<port>.app.github.dev',
+  //     //   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  //     //   'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+  //     //   'Access-Control-Allow-Credentials': 'true', // if you use cookies
+  //     // }
+  //   }
+  // },
+  dev: {
+    assetPrefix: `https://${loginCodespaceName}-${loginPort}.app.github.dev/`,
+    port: loginPort,
+  },
   runtime: {
     router: true,
   },
